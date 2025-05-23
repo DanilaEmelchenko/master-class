b4cc23ad83d7ba6499fc96debeab454faffe0820
Danila Emelchenko <emelchenko1997@yandex.ru>
2025-05-23 14:28:34 +0300

    Seeding the Supabase Remote Database with Fakerjs

--- database/seed.js
+++ database/seed.js
@@ -2,4 +2,17 @@ import { fakerEN_US as faker } from '@faker-js/faker'
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
-console.log(supabase)
+
+const seedProjects = async () => {
+  const name = faker.lorem.words(3)
+  const projectData = {
+    name: name,
+    slug: name.toLocaleUpperCase().replace(/ /g, '-'),
+    status: faker.helpers.arrayElement(['in-progress', 'completed']),
+    collaborators: [1, 2, 3],
+  }
+
+  await supabase.from('projects').insert(projectData)
+}
+
+await seedProjects()
