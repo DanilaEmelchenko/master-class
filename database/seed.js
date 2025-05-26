f752a0e63e22894cf5f1c7b6dac8fa8aca8e16ca
Danila Emelchenko <emelchenko1997@yandex.ru>
2025-05-26 13:41:01 +0300

    Insert Bulk Entries Into Supabase Database

--- database/seed.js
+++ database/seed.js
@@ -3,16 +3,22 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
 
-const seedProjects = async () => {
-  const name = faker.lorem.words(3)
-  const projectData = {
-    name: name,
-    slug: name.toLocaleUpperCase().replace(/ /g, '-'),
-    status: faker.helpers.arrayElement(['in-progress', 'completed']),
-    collaborators: [1, 2, 3],
+const seedProjects = async (numEntries) => {
+  const projects = []
+
+  for (let i = 0; i < numEntries; i++) {
+    const name = faker.lorem.words(3)
+    const projectData = {
+      name: name,
+      slug: name.toLocaleUpperCase().replace(/ /g, '-'),
+      status: faker.helpers.arrayElement(['in-progress', 'completed']),
+      collaborators: [1, 2, 3],
+    }
+
+    projects.push(projectData)
   }
 
-  await supabase.from('projects').insert(projectData)
+  await supabase.from('projects').insert(projects)
 }
 
-await seedProjects()
+await seedProjects(10)
