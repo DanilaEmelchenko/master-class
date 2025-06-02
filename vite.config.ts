de8752c2437009cb6d919bbb615236541309d8e9
Danila Emelchenko <emelchenko1997@yandex.ru>
2025-06-02 12:10:04 +0300

    Integrated taiwlind css via postcss

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import VueRouter from 'unplugin-vue-router/vite'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -18,7 +17,6 @@ export default defineConfig({
     }),
     vueDevTools(),
     VueRouter(),
-    tailwindcss(),
   ],
   resolve: {
     alias: {
