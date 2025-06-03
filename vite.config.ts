feef75645ef1719710ed5e6eb868957e124eb72a
Danila Emelchenko <emelchenko1997@yandex.ru>
2025-06-03 12:31:23 +0300

    Integrate unplugin-auto-import with Vue and Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import VueRouter from 'unplugin-vue-router/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -17,6 +18,18 @@ export default defineConfig({
     }),
     vueDevTools(),
     VueRouter(),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.vue\.[tj]sx?\?vue/, // .vue (vue-loader with experimentalInlineMatchResource enabled)
+        /\.md$/, // .md
+      ],
+      imports: ['vue', 'vue-router'],
+      dts: true,
+      viteOptimizeDeps: true,
+    }),
   ],
   resolve: {
     alias: {
