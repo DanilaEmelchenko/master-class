74e5c9d78dfed8684e5dc83bc0f298603c5f1197
Danila Emelchenko <emelchenko1997@yandex.ru>
2025-08-01 15:27:15 +0300

    Create a Composable for Handling Form Errors

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,13 +1,15 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
+import { useFormErrors } from '@/composables/formErrors'
 import { login } from '@/utils/supaAuth'
+import type { AuthError } from '@supabase/supabase-js'
 
 const formData = ref({
   email: '',
   password: '',
 })
 
-const _error = ref('')
+const { serverError, handleServerError } = useFormErrors()
 
 const router = useRouter()
 
@@ -15,8 +17,7 @@ const signin = async () => {
   const { error } = await login(formData.value)
   if (!error) return router.push('/')
 
-  _error.value =
-    error.message === 'Invalid login credentials' ? 'Incorrect email or password' : error.message
+  handleServerError(error)
 }
 </script>
 
@@ -41,7 +42,7 @@ const signin = async () => {
               placeholder="johndoe19@example.com"
               required
               v-model="formData.email"
-              :class="{ 'border-red-500': _error }"
+              :class="{ 'border-red-500': serverError }"
             />
           </div>
           <div class="grid gap-2">
@@ -55,11 +56,11 @@ const signin = async () => {
               autocomplete
               required
               v-model="formData.password"
-              :class="{ 'border-red-500': _error }"
+              :class="{ 'border-red-500': serverError }"
             />
           </div>
-          <ul class="text-sm text-left text-red-500" v-if="_error">
-            <li class="list-disc">{{ _error }}</li>
+          <ul class="text-sm text-left text-red-500" v-if="serverError">
+            <li class="list-disc">{{ serverError }}</li>
           </ul>
           <Button type="submit" class="w-full"> Login </Button>
         </form>
